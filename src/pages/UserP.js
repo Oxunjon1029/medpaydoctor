4dbf5bb5909187eea9c5f6561fd63fd6c0d7f4b7
Пользователь <Oxunjon Abdusalomov>
2022-05-31 20:57:58 +0500

    some

--- src/pages/UserP.js
+++ src/pages/UserP.js
@@ -51,7 +51,7 @@ const UserP = () => {
       setDoctorBirthDate(birth)
       form.setFieldsValue(essentialData);
     });
-  }, [form]);
+  }, [form, academicTitle]);
   const [lang, setLang] = useState([]);
 
   // doctorget and getlanguage useEffect
@@ -88,7 +88,7 @@ const UserP = () => {
         setSpecializationDirections(res.data);
       })
     }).catch((err) => { })
-  }, [academicTitle])
+  }, [])
   // upload img
   const [fileList, setFileList] = useState([]);
   const [previewVisible, setPreviewVisible] = useState(false);
